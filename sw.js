379d5db51e870cacf131e8e600025d3bfb6a47c2
mwdekdee <mwdekdee@gmail.com>
2026-08-31 07:00:43 +0700

    add service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'salary-app-v4';
+const CACHE = 'salary-app-v5';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
